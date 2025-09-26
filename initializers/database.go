99f6528965d9a32f01a77b2e252672e5ed3f1e14
aloneen <seisenbek2005@mail.ru>
2025-09-26 15:16:23 +0500

    Added database connection

---
+++ initializers/database.go
@@ -0,0 +1,35 @@
+package initializers
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+var DB *sql.DB
+
+func ConnectToDB() (*sql.DB, error) {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	database := os.Getenv("DB_NAME")
+
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, database,
+	)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	DB = db
+	return db, nil
+
+}
